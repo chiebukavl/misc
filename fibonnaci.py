2540375d43e12d35258dbd03fc9886fb3e9859a0
Chiebuka V. Lebechi <chiebuka.lebechi@gmail.com>
2020-05-20 18:08:31 -0500

    initial commit for fibonnaci and rpsGame

---
+++ fibonnaci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 20:02:36 2020
+
+@author: chiebukavl
+"""
+
+
+    
+    
+    
---
+++ rpsGame.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+name: rpsGame.py
+description: rock, paper, scissors game
+modified: 17 MAY 2020
+@author: chiebukavl
+"""
+
+import random, sys
+
+wins = 0
+losses = 0
+ties = 0
+
